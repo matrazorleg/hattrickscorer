f807f4d18c48c5799b7ab5e74492b70134a6be68
Matteo <matteo.gambirasio.dev@gmail.com>
2023-10-18 11:18:04 +0200

    mailer

--- mailer.php
+++ mailer.php
@@ -14,17 +14,17 @@ if($_POST["email"] != "" && $_POST["name"] != "" && $_POST["message"] != "")
 	//incredibilmente dava problemi...
 	$mail->isSMTP();                                      // Set mailer to use SMTP
 	
-	$mail->Host = 'smtp.gmail.com';  			  // Specify main and backup SMTP servers
+	$mail->Host = 'xxx';  			  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'htscorer@gmail.com';               // SMTP username
-	$mail->Password = 'kawasakier6n';                         // SMTP password
+	$mail->Username = 'xxx';               // SMTP username
+	$mail->Password = 'xxx';                         // SMTP password
 	
 	//parametri di sviluppo
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
 	
 	$mail->setFrom($_POST["email"], $_POST["name"]);
-	$mail->addAddress('htscorer@gmail.com', 'Hattrick Scorer Team');     // Add a recipient
+	$mail->addAddress('xxx', 'Hattrick Scorer Team');     // Add a recipient
 	$mail->addReplyTo($_POST["email"], $_POST["name"]);
 
 	$mail->Subject = 'Mail from HattrickScorer.com';
